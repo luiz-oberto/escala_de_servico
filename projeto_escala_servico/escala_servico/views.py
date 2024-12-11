aab4e896be02027133b94b3bb8690c114580b08d
Luiz Oberto <lom.raiol@gmail.com>
2024-12-11 18:15:18 -0300

    Weekday appears with monthday.

--- projeto_escala_servico/escala_servico/views.py
+++ projeto_escala_servico/escala_servico/views.py
@@ -3,49 +3,19 @@ from escala_servico.models import Militar, Escala
 from datetime import date, timedelta
 from django.db.models import Q
 import calendar
+import locale
+
+locale.setlocale(locale.LC_TIME, "pt_br.UTF-8")
 
 data_hoje = date.today()
 mes_atual = data_hoje.month
 ano_atual = data_hoje.year
-dia_da_semana = data_hoje.isoweekday()
-
-# Função para deletar a escala do mês anterior e gerar nova
-def atualizar_escala():
-    # altera o dia data atual para o primeiro dia do mês
-    inicio_mes_atual = data_hoje.replace(day=1)
-    quantidade_de_dias = calendar.monthrange(ano_atual, mes_atual)[1]
-
-    # Verificar se existe escala para o mês atual
-    escala_mes_atual = Escala.objects.filter(mes_referencia=inicio_mes_atual)
-
-    if not escala_mes_atual.exists():
-        # Apagar a escala do mês anterior
-        Escala.objects.all().delete()
-
-        # Obter as pessoas e gerar nova escala para o mês
-        militares = list(Militar.objects.order_by('-antiguidade'))
- 
-
-        i = 0
-        while i < quantidade_de_dias:
-            data_escala = inicio_mes_atual + timedelta(days=i) # soma mais um dia
-            print(data_escala)
-            pessoa_escalada = militares[i % len(militares)]
-            print('Pessoa escalada: ',pessoa_escalada)
-            Escala.objects.create(data=data_escala, pessoa=pessoa_escalada, mes_referencia=inicio_mes_atual)
-            i+=1
-
-    return Escala.objects.filter(mes_referencia=inicio_mes_atual)
-
-
 
 def escala(request):
-    print(dia_da_semana)
-    escala = atualizar_escala()
+    atualizar_escala()
     data_escala = Escala.objects.order_by('data')
 
     meses_do_ano = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-
     mes = meses_do_ano[mes_atual-1]
 
     
@@ -56,3 +26,46 @@ def escala(request):
     }
 
     return render(request, 'escalas/escala_do_mes.html', context)
+
+
+
+# Função para gerar um nova escala e deletar a do mês anterior quando necessário
+def atualizar_escala():
+    # altera o dia data atual para o primeiro dia do mês
+    inicio_mes_atual = data_hoje.replace(day=1)
+    quantidade_de_dias = calendar.monthrange(ano_atual, mes_atual)[1]
+
+    # Verificar se existe escala para o mês atual
+    escala_mes_atual = Escala.objects.filter(mes_referencia=inicio_mes_atual)
+    if not escala_mes_atual.exists():
+        # Apagar a escala do mês anterior
+        Escala.objects.all().delete()
+        # Pegar os dias da semana desse mês
+        dias_da_semana = generate_weekday(ano_atual, mes_atual)
+        # Obter as pessoas e gerar nova escala para o mês
+        militares = list(Militar.objects.order_by('-antiguidade'))
+ 
+
+        i = 0
+        while i < quantidade_de_dias:
+            data_escala = inicio_mes_atual + timedelta(days=i) # soma mais um dia
+            pessoa_escalada = militares[i % len(militares)]
+            Escala.objects.create(data=data_escala, pessoa=pessoa_escalada, dias_da_semana=dias_da_semana[i] ,  mes_referencia=inicio_mes_atual) #
+            i+=1
+
+    return Escala.objects.filter(mes_referencia=inicio_mes_atual)
+
+
+
+def generate_weekday(ano_atual, mes_atual):
+    # Obter o número de dias no mês de dezembro
+    from calendar import monthrange
+    dias_no_mes = monthrange(ano_atual, mes_atual)[1]
+
+    # Gerar uma lista com os dias da semana por extenso
+    dias_da_semana = [
+        (date(ano_atual, mes_atual, dia).strftime("%A"))  # Data e nome do dia
+        for dia in range(1, dias_no_mes + 1)
+    ]
+
+    return dias_da_semana
