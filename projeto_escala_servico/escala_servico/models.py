6eec85bf83b49b6dfd71e46b79fe2e69f1bf297c
Luiz Oberto <lom.raiol@gmail.com>
2024-12-13 11:36:05 -0300

    Implementacao das funcoes atualizar_escala e gererate_weekday na classe Escala.

--- projeto_escala_servico/escala_servico/models.py
+++ projeto_escala_servico/escala_servico/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from datetime import date, timedelta
+import calendar
+import locale
+
+
+locale.setlocale(locale.LC_TIME, "pt_br.UTF-8")
 
 # Create your models here.
 # mudar para Pessoal
 class Militar(models.Model):
     nome_de_guerra = models.CharField(max_length=50)
     graduacao = models.CharField(max_length=10)
-    antiguidade = models.CharField(max_length=4)
+    antiguidade = models.IntegerField()
     # disponibilidadde = models.BooleanField(default=True)
     ultimo_a_dar_servico = models.BooleanField(default=False)
 
@@ -17,13 +23,87 @@ class Militar(models.Model):
 
     def __str__(self) -> str:
         return f'{self.nome_de_guerra}'
+
+
+
     
 class Escala(models.Model):
     data = models.DateField(unique=True)
     dias_da_semana = models.CharField(max_length=50)
     pessoa = models.ForeignKey('Militar', on_delete=models.CASCADE, null=True, blank=True)
     mes_referencia = models.DateField(null=True, blank=True)
+    # mes = atributo que salvará o mês da escala
+    # ano = atributo que vai salvar o ano da escala
 
     def __str__(self):
         return f'{self.data} - {self.pessoa}'
+
+    @classmethod
+    def atualizar_escala(cls):
+        data_hoje = date.today()
+        ano_atual = data_hoje.year
+        mes_atual = data_hoje.month
+
+        ##### DATAS PARA TESTES #########
+        # data_hoje = date(2025, 2, 1)
+        # ano_atual = data_hoje.year
+        # mes_atual = data_hoje.month
+
+        # Altera o dia data atual para o primeiro dia do mês
+        inicio_mes_atual = data_hoje.replace(day=1)
+        quantidade_de_dias = calendar.monthrange(ano_atual, mes_atual)[1]
+
+        # Verificar se existe escala para o mês atual
+        escala_mes_atual = cls.objects.filter(mes_referencia=inicio_mes_atual)
+        if not escala_mes_atual.exists():
+            # Apagar a escala do mês anterior
+            cls.objects.all().delete()
+
+            # Pegar os dias da semana desse mês
+            dias_da_semana = cls.generate_weekday(ano_atual, mes_atual)
+
+            # Obter as pessoas e gerar nova escala para o mês
+            militares = list(Militar.objects.order_by('-antiguidade'))
+
+            indice = 0
+            indice_militar = 0
+            while indice < quantidade_de_dias:
+                data_escala = inicio_mes_atual + timedelta(days=indice)  # Soma mais um dia
+                pessoa_escalada = militares[indice_militar % len(militares)]
+
+                if dias_da_semana[indice] in ['sábado', 'domingo']:
+                    cls.objects.create(data=data_escala, dias_da_semana=dias_da_semana[indice], mes_referencia=inicio_mes_atual)
+                else:
+                    cls.objects.create(data=data_escala, pessoa=pessoa_escalada, dias_da_semana=dias_da_semana[indice], mes_referencia=inicio_mes_atual)
+
+                indice += 1
+                if dias_da_semana[indice - 1] not in ['sábado', 'domingo']:
+                    indice_militar += 1
+
+        return data_hoje, cls.objects.filter(mes_referencia=inicio_mes_atual)
+
+    @staticmethod
+    def generate_weekday(ano_atual, mes_atual):
+        # Obter o número de dias no mês de dezembro
+        from calendar import monthrange
+        dias_no_mes = monthrange(ano_atual, mes_atual)[1]
+
+        # Gerar uma lista com os dias da semana por extenso
+        dias_da_semana = [
+            (date(ano_atual, mes_atual, dia).strftime("%A"))  # Data e nome do dia
+            for dia in range(1, dias_no_mes + 1)
+        ]
+        
+        # Ajusta o nome dos dias da semana
+        for dia in dias_da_semana:
+            if dia == 'terÃ§a-feira':
+                index = dias_da_semana.index('terÃ§a-feira')
+                dias_da_semana.pop(index)
+                dias_da_semana.insert(index, 'terça-feira')
+            elif dia == 'sÃ¡bado':
+                index = dias_da_semana.index('sÃ¡bado')
+                dias_da_semana.pop(index)
+                dias_da_semana.insert(index, 'sábado')
+
+        return dias_da_semana
 
--- projeto_escala_servico/escala_servico/views.py
+++ projeto_escala_servico/escala_servico/views.py
@@ -7,37 +7,14 @@ import locale
 
 locale.setlocale(locale.LC_TIME, "pt_br.UTF-8")
 
-# data_hoje = date.today()
-# mes_atual = data_hoje.month
-# ano_atual = data_hoje.year
+data_hoje = date.today()
+mes_atual = data_hoje.month
+ano_atual = data_hoje.year
 
-# DATAS PARA TESTES 
-data_hoje = date(2025, 1, 1)
-mes_atual = 1
-ano_atual = 2025
 
 def escala(request):
-    ########################################################################
-    # LÓGICA PARA ACERTAR A LISTA DO ÚLTIMO QUE DEU SERVIÇO
-    militares = list(Militar.objects.order_by('-antiguidade'))
-    lista_atualizada = []
-    # ultimo_que_deu_servico = ''
-    for militar in militares:
-        if militar.ultimo_a_dar_servico == True:
-            print(f'{militar.nome_de_guerra} foi o último a dar serviço')
-            ultimo_que_deu_servico = militar.nome_de_guerra
-        else:
-            print(f'{militar.nome_de_guerra} não foi o último')
-            lista_atualizada.append(militar.nome_de_guerra)
-    lista_atualizada.append(ultimo_que_deu_servico)
-    print(lista_atualizada)
-        
-
-    ########################################################################
-
     # Generate duty roster
-    atualizar_escala()
-    # print(data_hoje)
+    Escala.atualizar_escala()
 
     # fetch duty roster
     data_escala = Escala.objects.order_by('data')
@@ -56,64 +33,18 @@ def escala(request):
 
 
 
-# Função para gerar um nova escala e deletar a do mês anterior quando necessário
-def atualizar_escala():
-    # altera o dia data atual para o primeiro dia do mês
-    inicio_mes_atual = data_hoje.replace(day=1)
-    quantidade_de_dias = calendar.monthrange(ano_atual, mes_atual)[1]
+def verify_last_duty():
+    # LÓGICA PARA ACERTAR A LISTA DO ÚLTIMO QUE DEU SERVIÇO
+    militares = list(Militar.objects.order_by('-antiguidade'))
+    lista_atualizada = []
+    for militar in militares:
+        if militar.ultimo_a_dar_servico == True:
+            # print(f'{militar.nome_de_guerra} foi o último a dar serviço')
+            ultimo_que_deu_servico = militar.nome_de_guerra
+        else:
+            # print(f'{militar.nome_de_guerra} não foi o último')
+            lista_atualizada.append(militar.nome_de_guerra)
 
-    # Verificar se existe escala para o mês atual
-    escala_mes_atual = Escala.objects.filter(mes_referencia=inicio_mes_atual)
-    if not escala_mes_atual.exists():
-        # Apagar a escala do mês anterior
-        Escala.objects.all().delete()
-        # Pegar os dias da semana desse mês
-        dias_da_semana = generate_weekday(ano_atual, mes_atual)
-        # Obter as pessoas e gerar nova escala para o mês
-        militares = list(Militar.objects.order_by('-antiguidade'))
-
-
- 
-
-        indice = 0
-        indice_militar = 0
-        while indice < quantidade_de_dias:
-            data_escala = inicio_mes_atual + timedelta(days=indice) # soma mais um dia
-            pessoa_escalada = militares[indice_militar % len(militares)]
-            if dias_da_semana[indice] == 'sábado' or dias_da_semana[indice] == 'domingo':
-                Escala.objects.create(data=data_escala, dias_da_semana=dias_da_semana[indice],  mes_referencia=inicio_mes_atual)
-                indice+=1
-            else:
-                Escala.objects.create(data=data_escala, pessoa=pessoa_escalada, dias_da_semana=dias_da_semana[indice] ,  mes_referencia=inicio_mes_atual)
-                indice+=1
-                indice_militar+=1
-
-    return Escala.objects.filter(mes_referencia=inicio_mes_atual)
-
-
-
-def generate_weekday(ano_atual, mes_atual):
-    # Obter o número de dias no mês de dezembro
-    from calendar import monthrange
-    dias_no_mes = monthrange(ano_atual, mes_atual)[1]
-
-    # Gerar uma lista com os dias da semana por extenso
-    dias_da_semana = [
-        (date(ano_atual, mes_atual, dia).strftime("%A"))  # Data e nome do dia
-        for dia in range(1, dias_no_mes + 1)
-    ]
-    # print(dias_da_semana.index('domingo'))
-    for dia in dias_da_semana:
-        # print(dia)
-        if dia == 'terÃ§a-feira':
-            index = dias_da_semana.index('terÃ§a-feira')
-            dias_da_semana.pop(index)
-            dias_da_semana.insert(index, 'terça-feira')
-        elif dia == 'sÃ¡bado':
-            index = dias_da_semana.index('sÃ¡bado')
-            dias_da_semana.pop(index)
-            dias_da_semana.insert(index, 'sábado')
-
-    print(dias_da_semana)
-
-    return dias_da_semana
+    lista_atualizada.append(ultimo_que_deu_servico)
+    return lista_atualizada
+        
